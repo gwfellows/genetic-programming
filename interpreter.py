032eca2e340380bd9539dc76c26f562f06433f1c
Grant Fellows <gwfellows@icloud.com>
2022-03-22 20:28:00 -0700

    add graph printing

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,8 @@
 import random
 import math
 from inspect import signature
+import graphviz
+
 
 def head(exp):
     return exp[0]
@@ -11,26 +13,25 @@ def tail(exp):
 def nargs(func):
     return len(signature(func).parameters)
 
-def interpret(exp):
+def interpret(exp, definitions={}):
     if (type(exp) is int) or (type(exp) is float):
         return exp
-    else:
-        return head(exp)(
-            *map(interpret, tail(exp))
-            )
+    if type(exp) is str:
+        return definitions[exp]
+    return head(exp)(
+        *map(lambda e: interpret(e, definitions), tail(exp))
+        )
 
-def prettyprint(exp, indent=0):
+def asciiprint(exp, indent=0):
     if (type(exp) is int) or (type(exp) is float):
-        print(" "*indent+str(exp))
+        print("|"*indent+str(exp))
     elif callable(exp):
-        print(" "*indent+exp.__name__)
+        print("|"*indent+exp.__name__)
     else:
-        prettyprint(head(exp), indent)
+        asciiprint(head(exp), indent)
         for arg in tail(exp):
-            prettyprint(arg, indent+3)
+            asciiprint(arg, indent+1)
     
-
-            
 def add(a, b):
     return a+b;
 
@@ -60,28 +61,37 @@ def ln(a):
 
 FUNCTIONS = {add, sub, mul, div, sqrt, exp, ln}
 TERMINALS = {0,1,2,3,4,5,6,7,8,9}
-BOTH = list(FUNCTIONS.union(TERMINALS))
-#print(BOTH)
 
-def randexp(atom):
-    if atom in TERMINALS:
-        return atom
-    if atom in FUNCTIONS:
-        return (atom, *[randexp(random.choice(BOTH)) for _ in range(nargs(atom))])
+def randexp(F, T):
+    U = list(F.union(T))
+    def _randexp(atom):
+        print(atom)
+        if atom in T:
+            return atom
+        if atom in F:
+            return (atom, *[_randexp(random.choice(U)) for _ in range(nargs(atom))])
+    return _randexp(random.choice(list(F)))
+    
+_count = 0
+def _newname():
+    global count
+    _count += 1
+    return "CHILD"+str(count)
 
+def graphprint(exp):
+    dot = graphviz.Digraph()
 
-prettyprint(
-randexp(add)
-    )
-
-"""
-print(
-    interpret(
-        (add, (sub, 9, 1), (mul, 20, (div, 9, 6))),
-    )
-)
-
-prettyprint(
-    (add, (sub, 9, 1), (mul, 20, (div, 9, 6))),
-)
-"""
+    def _graphprint(exp, parentname):
+        if type(exp) is tuple:
+            for child in tail(exp):
+                childname = _newname()
+                dot.node(
+                    childname,
+                    head(child).__name__ if (type(child) is tuple) else str(child)
+                    )
+                dot.edge(parentname, childname)
+                _graphprint(child, childname)
+            
+    dot.node("PARENT", head(exp).__name__)
+    _graphprint(exp, "PARENT")
+    dot.render('graph-output/graph.gv', view=True)
