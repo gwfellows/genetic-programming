73f5c52bd722114b7a38cd856f07b8f83bb6b70b
Grant Fellows <gwfellows@icloud.com>
2022-05-31 21:56:01 -0700

    presentation

--- presentation/drawgraphs.py
+++ presentation/drawgraphs.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import csv
 citation = "Oosterbaan, R.J.. (2019). CROP TOLERANCE TO SOIL SALINITY, STATISTICAL ANALYSIS OF DATA MEASURED IN FARM LANDS."
 data = []
-with open('log10 transistors-per-microprocessor.csv', mode='r') as d:
+with open('babyheights.csv', mode='r') as d:
     reader = csv.reader(d)
     data = [(float(rows[0]),float(rows[1])) for rows in reader]
 
@@ -12,7 +12,8 @@ y = [pair[1] for pair in data]
 plt.scatter(x,y, marker="o")
 import math
 def f(a):
-    return 0.1568459619399607*a - 306.00613944659269
+    #0.337X+11.1\ln\left(X+1.9\right)+43
+    return 0.337*a+11.1*math.log(a+1.9)+43
 
 x2 = []
 y2 = []
@@ -28,8 +29,8 @@ for x in range(0,1000):
 #0.1568459619399607 X - 306.00613944659269
     
 plt.plot(x2,y2,color="red",linestyle="--")
-plt.ylabel("log transistors per microprocessor")
-plt.xlabel("year")
+plt.ylabel("50th percentile height")
+plt.xlabel("months alive")
 #plt.title(citation)
 plt.show()
 
