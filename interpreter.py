b1a9e42af0eb337c686c89d07e9410cd8de3c55f
Grant Fellows <33993581+gwfellows@users.noreply.github.com>
2023-11-19 15:51:37 -0500

    add images

--- interpreter.py
+++ interpreter.py
@@ -3,14 +3,18 @@ import math
 from inspect import signature
 import graphviz
 
+
 def head(exp):
     return exp[0]
+
 
 def tail(exp):
     return exp[1:]
 
+
 def nargs(func):
     return len(signature(func).parameters)
+
 
 def interpret(exp, definitions=None):
     if (type(exp) is int) or (type(exp) is float):
@@ -18,75 +22,94 @@ def interpret(exp, definitions=None):
     if type(exp) is str:
         return definitions[exp]
     try:
-        return head(exp)(
-            *map(lambda e: interpret(e, definitions), tail(exp))
-            )
+        return head(exp)(*map(lambda e: interpret(e, definitions), tail(exp)))
     except OverflowError:
         return 0
 
+
 def _simplify(exp):
-    if (type(exp) in (int,str,float)):
+    if type(exp) in (int, str, float):
         return exp
-    if all((type(i) in (str,int,float)) for i in tail(exp)) and any((type(i) is str) for i in tail(exp)):
+    if all((type(i) in (str, int, float)) for i in tail(exp)) and any(
+        (type(i) is str) for i in tail(exp)
+    ):
         return exp
     try:
-        return head(exp)(
-            *map(simplify, tail(exp))
-            )
+        return head(exp)(*map(simplify, tail(exp)))
     except OverflowError:
         return 0
+
 
 def wrapper(func):
     def newfunc(*args):
-        if any(type(i) not in (int,float) for i in args):
-            return [func,*args]
+        if any(type(i) not in (int, float) for i in args):
+            return [func, *args]
         return func(*args)
-        
+
     return newfunc
-    
+
+
 def simplify(exp):
-    if type(exp) in (tuple,list):
-        return wrapper(head(exp))(
-            *map(simplify, tail(exp))
-            )
+    if type(exp) in (tuple, list):
+        return wrapper(head(exp))(*map(simplify, tail(exp)))
     else:
         return exp
+
 
 def asciiprint(exp):
     def _asciiprint(exp, indent=0):
         if (type(exp) is int) or (type(exp) is float) or (type(exp) is str):
-            return "|"*indent+str(exp)
+            return "|" * indent + str(exp)
         elif callable(exp):
-            return "|"*indent+exp.__name__
+            return "|" * indent + exp.__name__
         else:
-            return "\n".join((
-                _asciiprint(head(exp), indent),
-                *(_asciiprint(arg, indent+1) for arg in tail(exp)),
-                ))
+            return "\n".join(
+                (
+                    _asciiprint(head(exp), indent),
+                    *(_asciiprint(arg, indent + 1) for arg in tail(exp)),
+                )
+            )
+
     return _asciiprint(exp, indent=0)
+
 
 def _handle(t):
     return t() if callable(t) else t
-    
+
+
 def randexp(F, T, maxdepth=5):
     U = list(F.union(T))
+
     def _randexp(atom, depth=2):
         if atom in T:
             return _handle(atom)
         if atom in F:
-            return (atom, *[_randexp(
-                random.choice(U) if depth<maxdepth else random.choice(list(T)), depth+1
-                ) for _ in range(nargs(atom))])
+            return (
+                atom,
+                *[
+                    _randexp(
+                        random.choice(U)
+                        if depth < maxdepth
+                        else random.choice(list(T)),
+                        depth + 1,
+                    )
+                    for _ in range(nargs(atom))
+                ],
+            )
+
     return _randexp(random.choice(list(F)))
 
-    
+
 _count = 0
+
+
 def _newname():
     global _count
     _count += 1
-    return "CHILD"+str(_count)
+    return "CHILD" + str(_count)
 
-#print a tree to a graph
+
+# print a tree to a graph
 def graphprint(exp, name="graph.gv"):
     dot = graphviz.Digraph()
 
@@ -96,58 +119,70 @@ def graphprint(exp, name="graph.gv"):
                 childname = _newname()
                 dot.node(
                     childname,
-                    head(child).__name__ if (type(child) in (tuple, list)) else str(child)
-                    )
+                    head(child).__name__
+                    if (type(child) in (tuple, list))
+                    else str(child),
+                )
                 dot.edge(parentname, childname)
                 _graphprint(child, childname)
-            
+
     dot.node("PARENT", head(exp).__name__)
     _graphprint(exp, "PARENT")
-    dot.render('graph-output/'+name, view=True)
+    dot.render("graph-output/" + name, view=True)
+
 
 # return the indexes for all subtrees of an expression, to use in crossover
 # ex. for the expression [+ [ln 2] [* 2 3]] it would return:
-# (1,) , (2,) , (1,1) , (2,1) , (2,2) 
+# (1,) , (2,) , (1,1) , (2,1) , (2,2)
 # as there are subtrees at exp[1], exp[2], exp[1][1], exp[2][1], and exp[2][2] but no more
 def get_subindices(exp):
     ret = []
-    curr = [(_,) for _ in range(1,1+nargs(exp[0]))]
+    curr = [(_,) for _ in range(1, 1 + nargs(exp[0]))]
     while len(curr) > 0:
         for i, idxs in enumerate(curr):
             subtree = access_subindex(exp, idxs)
             if type(subtree) in (tuple, list):
                 ret.append(idxs)
                 del curr[i]
-                for i in range(1,1+nargs(subtree[0])):
-                    curr.append(idxs+(i,))
+                for i in range(1, 1 + nargs(subtree[0])):
+                    curr.append(idxs + (i,))
             else:
                 ret.append(idxs)
                 del curr[i]
-    #ret.append((slice(None),)) #can also get full tree
+    # ret.append((slice(None),)) #can also get full tree
     return ret
+
 
 # return a radom subindex of a expression
 def random_subindex(exp):
     return random.choice(get_subindices(exp))
 
+
 # access a subtree of an index by indices
-# ex. access_subindex(exp, (1,3,4,2,1)) would return exp[1][3][4][2][1] 
+# ex. access_subindex(exp, (1,3,4,2,1)) would return exp[1][3][4][2][1]
 def access_subindex(exp, idxs):
-    ret = exp#[:]
+    ret = exp  # [:]
     for i in idxs:
         ret = ret[i]
     return ret
 
-#recreate array, replacing item at idxs with newval
+
+# recreate array, replacing item at idxs with newval
 def with_subindex(exp, idxs, newval):
     if len(idxs) == 0:
         return newval
     else:
-        return [*exp[:idxs[0]], with_subindex(exp[idxs[0]], idxs[1:], newval), *exp[idxs[0]+1:]]
+        return [
+            *exp[: idxs[0]],
+            with_subindex(exp[idxs[0]], idxs[1:], newval),
+            *exp[idxs[0] + 1 :],
+        ]
+
 
 import copy
 
-#swap random subtrees between 2 trees
+
+# swap random subtrees between 2 trees
 def crossover(exp1, exp2):
     sub_1 = random_subindex(exp1)
     sub_2 = random_subindex(exp2)
@@ -156,86 +191,102 @@ def crossover(exp1, exp2):
     exp2 = with_subindex(exp2, sub_2, access_subindex(oldexp1, sub_1))
     return exp1, exp2
 
-def mutate(exp,F,T):
-    return with_subindex(exp, random_subindex(exp), randexp(F,T,3))
+
+def mutate(exp, F, T):
+    return with_subindex(exp, random_subindex(exp), randexp(F, T, 3))
+
 
 import matplotlib.pyplot as plt
+
 
 # the main evolution loop
 # attemps to evolve a tree to minimize the value of the fitness function, terminating when the selection cutoff is reached or 500 generations pass
 # note that the selection cutoff uses normalized fitness ( 1/(1+raw fitness) ), so 1 is perfect
 # if "verbose" is True on this will print out it's progress and plot it at the end
 # templates
-def evolve(functions, 
-    terminals, 
-    fitness_function, 
-    pop_size=50, 
-    init_max_depth=10, 
-    crossover_rate=0.5, 
-    selection_cutoff=0.99, 
+def evolve(
+    functions,
+    terminals,
+    fitness_function,
+    pop_size=50,
+    init_max_depth=10,
+    crossover_rate=0.5,
+    selection_cutoff=0.99,
     verbose=True,
-    gens_cutoff = 500_000,
+    gens_cutoff=500_000,
     mutation_rate=0,
-    templates=((1, 'RANDOM'),),
-    updatefreq=10):  
-
-    
+    templates=((1, "RANDOM"),),
+    updatefreq=10,
+):
     names = [t[1] for t in templates]
     weights = [t[0] for t in templates]
     population = []
     for _ in range(pop_size):
         name = random.choices(names, weights=weights)[0]
-        if name=='RANDOM':
+        if name == "RANDOM":
             population.append(randexp(functions, terminals, init_max_depth))
         else:
             population.append(name())
-        
+
     xs = []
     ys = []
-    import sys,os
+    import sys, os
+
     for _ in range(gens_cutoff):
         try:
-            fitnesses = list(map(lambda p: 1/(1+fitness_function(p)), population))
+            fitnesses = list(map(lambda p: 1 / (1 + fitness_function(p)), population))
             xs.append(_)
-            ys.append(1/(max(fitnesses))-1)
-            if verbose and _%updatefreq==0:
+            ys.append(1 / (max(fitnesses)) - 1)
+            if verbose and _ % updatefreq == 0:
                 os.system("cls")
                 print("matching slope...")
                 print()
                 print("GENERATION   NRMSE")
-                print('{:<12}'.format(_), 1/(max(fitnesses))-1)
+                print("{:<12}".format(_), 1 / (max(fitnesses)) - 1)
                 print()
                 print("CRTL+C to terminate")
-            if (1/(max(fitnesses))-1) < selection_cutoff:
+            if (1 / (max(fitnesses)) - 1) < selection_cutoff:
                 break
             new_pop = []
             new_pop.append(population[fitnesses.index(max(fitnesses))])
-            #print(sum(fitnesses))
+            # print(sum(fitnesses))
             while len(new_pop) < pop_size:
                 rand = random.random()
-                if rand<(mutation_rate+crossover_rate):
-                    if random.random()<(mutation_rate/(mutation_rate+crossover_rate)):
-                        new_pop.append(mutate(random.choices(population, weights = fitnesses, k=1)[0],functions,terminals))
+                if rand < (mutation_rate + crossover_rate):
+                    if random.random() < (
+                        mutation_rate / (mutation_rate + crossover_rate)
+                    ):
+                        new_pop.append(
+                            mutate(
+                                random.choices(population, weights=fitnesses, k=1)[0],
+                                functions,
+                                terminals,
+                            )
+                        )
                     else:
-                        for i in crossover(*random.choices(population, weights = fitnesses, k=2)):
+                        for i in crossover(
+                            *random.choices(population, weights=fitnesses, k=2)
+                        ):
                             new_pop.append(i)
                 else:
-                    new_pop.append(*random.choices(population, weights = fitnesses, k=1))
+                    new_pop.append(*random.choices(population, weights=fitnesses, k=1))
             population = new_pop
         except KeyboardInterrupt:
             break
-    
+
     if verbose:
         fig, ax = plt.subplots()
         ax.plot(xs, ys)
-        ax.set(xlabel='generation', ylabel='max fitness',
-               title='max fitness per generation')
+        ax.set(
+            xlabel="generation",
+            ylabel="max fitness",
+            title="max fitness per generation",
+        )
         ax.grid()
 
         fig.savefig("test.png")
-        plt.yscale('log')
+        plt.yscale("log")
         plt.show()
-    
-    #print(fitnesses.index(max(fitnesses)))
+
+    # print(fitnesses.index(max(fitnesses)))
     return population[fitnesses.index(max(fitnesses))]
-        
--- symreg.py
+++ symreg.py
@@ -8,89 +8,108 @@ import math
 import statistics
 import csv
 
-import matplotlib.pyplot as plt    
+import matplotlib.pyplot as plt
 
-my_parser = argparse.ArgumentParser(prog='symreg',description='Use symbolic regression to fit a curve to data')
-my_parser.add_argument('Path',
-                       metavar='path',
-                       type=str,
-                       help='the path to the data (csv format)')
-                       
-my_parser.add_argument('--suggest',
-                        action='store',
-                        choices=['linear','exponential','quadratic','poly3','poly4','log'],
-                        required=False,
-                        help='suggest a class of functions as the starting point for evolution')
+my_parser = argparse.ArgumentParser(
+    prog="symreg", description="Use symbolic regression to fit a curve to data"
+)
+my_parser.add_argument(
+    "Path", metavar="path", type=str, help="the path to the data (csv format)"
+)
 
-my_parser.add_argument('--psize',
-                        action='store',
-                        type=int,
-                        required=False,
-                        help='the population size to use (30 by default)')
+my_parser.add_argument(
+    "--suggest",
+    action="store",
+    choices=["linear", "exponential", "quadratic", "poly3", "poly4", "log"],
+    required=False,
+    help="suggest a class of functions as the starting point for evolution",
+)
 
-my_parser.add_argument('--updatefreq',
-                        action='store',
-                        type=int,
-                        required=False,
-                        help='how often to update the display (every 10 generations by default)')
+my_parser.add_argument(
+    "--psize",
+    action="store",
+    type=int,
+    required=False,
+    help="the population size to use (30 by default)",
+)
 
-my_parser.add_argument('--sizepenalty',
-                        action='store',
-                        type=float,
-                        required=False,
-                        help='how much to penalize large trees (0.001 by default)')
-                        
+my_parser.add_argument(
+    "--updatefreq",
+    action="store",
+    type=int,
+    required=False,
+    help="how often to update the display (every 10 generations by default)",
+)
+
+my_parser.add_argument(
+    "--sizepenalty",
+    action="store",
+    type=float,
+    required=False,
+    help="how much to penalize large trees (0.001 by default)",
+)
+
 args = my_parser.parse_args()
 
+
 def add(a, b):
-    ret = a+b
+    ret = a + b
     if math.isnan(ret) or math.isinf(ret):
         return 0
-    return ret;
+    return ret
+
 
 def sub(a, b):
-    return a-b;
+    return a - b
+
 
 def mul(a, b):
-    ret = a*b
+    ret = a * b
     if math.isnan(ret) or math.isinf(ret):
         return 0
-    return a*b;
+    return a * b
+
 
 def div(a, b):
-    if b==0:
+    if b == 0:
         return 0
-    ret = a/b
+    ret = a / b
     if math.isnan(ret) or math.isinf(ret):
         return 0
-    return a/b
+    return a / b
+
 
 def sqrt(a):
-    if a<0:
+    if a < 0:
         return 0
     return math.sqrt(a)
 
-def power(a,b):
+
+def power(a, b):
     a, b = float(a), float(b)
-    if a<=0:
+    if a <= 0:
         return 0
     ret = a**b
     if math.isnan(ret) or math.isinf(ret):
         return 0
     return ret
 
+
 def squared(a):
-    return power(a,2)
+    return power(a, 2)
+
 
 def ln(a):
-    if a<=0:
+    if a <= 0:
         return -1000000
     return math.log(a)
 
+
 def log(a, b):
-    if (a<=0) or (b<=1):
+    if (a <= 0) or (b <= 1):
         return -1000000
     return math.log(a, b)
+
 
 def cos(a):
     try:
@@ -98,145 +117,204 @@ def cos(a):
     except:
         return 0
 
-#print a tree as a expression
-#only works with binary operators +, *, / and -
-#and cosine, and the constant pi
+
+# print a tree as a expression
+# only works with binary operators +, *, / and -
+# and cosine, and the constant pi
 def expr_print(exp):
     if type(exp) in (str, int, float):
         if exp == math.pi:
             return "pi"
         return str(exp)
     if exp[0] == add:
-        return "("+expr_print(exp[1])+"+"+expr_print(exp[2])+")"
+        return "(" + expr_print(exp[1]) + "+" + expr_print(exp[2]) + ")"
     if exp[0] == mul:
-        return "("+expr_print(exp[1])+"*"+expr_print(exp[2])+")"
+        return "(" + expr_print(exp[1]) + "*" + expr_print(exp[2]) + ")"
     if exp[0] == sub:
-        return "("+expr_print(exp[1])+"-"+expr_print(exp[2])+")"
+        return "(" + expr_print(exp[1]) + "-" + expr_print(exp[2]) + ")"
     if exp[0] == div:
-        return "("+expr_print(exp[1])+"/"+expr_print(exp[2])+")"
+        return "(" + expr_print(exp[1]) + "/" + expr_print(exp[2]) + ")"
     if exp[0] == power:
-        return "("+expr_print(exp[1])+"**("+expr_print(exp[2])+"))"
+        return "(" + expr_print(exp[1]) + "**(" + expr_print(exp[2]) + "))"
     if exp[0] == cos:
-        return "cos("+expr_print(exp[1])+")"
+        return "cos(" + expr_print(exp[1]) + ")"
     if exp[0] == log:
-        return "log("+expr_print(exp[1])+" , "+expr_print(exp[2])+")"
+        return "log(" + expr_print(exp[1]) + " , " + expr_print(exp[2]) + ")"
     if exp[0] == ln:
-        return "ln("+expr_print(exp[1])+")"
+        return "ln(" + expr_print(exp[1]) + ")"
     if exp[0] == squared:
-        return "("+expr_print(exp[1])+"**(2))"
-        
+        return "(" + expr_print(exp[1]) + "**(2))"
+
+
 data = []
-#log10 transistors-per-microprocessor
-with open(args.Path, mode='r') as d:
+# log10 transistors-per-microprocessor
+with open(args.Path, mode="r") as d:
     reader = csv.reader(d)
-    data = [(float(rows[0]),float(rows[1])) for rows in reader]
+    data = [(float(rows[0]), float(rows[1])) for rows in reader]
+
 
 def get_differences(data):
     diffs = []
-    for i in range(1,len(data)):
+    for i in range(1, len(data)):
         x = data[i][0]
-        y = (data[i][1]-data[i-1][1])/(data[i][0]-data[i-1][0])
-        diffs.append([x,y])
+        y = (data[i][1] - data[i - 1][1]) / (data[i][0] - data[i - 1][0])
+        diffs.append([x, y])
     return diffs
-    
-#use NRMSE
+
+
+# use NRMSE
 data_diffs = get_differences(data)
 data_diffs_mean = statistics.mean(row[1] for row in data_diffs)
 
+
 def n_nodes(exp, d=0):
-    return sum(map(n_nodes,exp)) if type(exp) in (tuple, list) else 1
+    return sum(map(n_nodes, exp)) if type(exp) in (tuple, list) else 1
+
 
 if args.sizepenalty:
     sizepenalty = args.sizepenalty
 else:
     sizepenalty = 0.001
-    
-#n=0
+
+# n=0
+
 
 def score(exp):
     global n
     square_errors = []
-   
-    data_to_score = [[pair[0],interpreter.interpret(exp, {'X':pair[0]})] for pair in data]
+
+    data_to_score = [
+        [pair[0], interpreter.interpret(exp, {"X": pair[0]})] for pair in data
+    ]
     diffs_to_score = get_differences(data_to_score)
-    
+
     try:
         for i, pair in enumerate(diffs_to_score):
-            square_errors.append((diffs_to_score[i][1]-data_diffs[i][1])**2)
+            square_errors.append((diffs_to_score[i][1] - data_diffs[i][1]) ** 2)
     except OverflowError:
         print("overflow")
         return float("inf")
-    #import matplotlib.pyplot as plt            
+    # import matplotlib.pyplot as plt
 
-    #plt.plot([i for i in range(0,36)], [interpreter.interpret(exp, {'X':i}) for i in range(0,36)])
-    '''if random.random()<0.05:
-        print(expr_print(exp)+"\n"+str(math.sqrt(statistics.mean(square_errors))/abs(data_diffs_mean) + sizepenalty*n_nodes(exp)))'''
-    #plt.show()
-    #plt.savefig("GRPH"+str(n)+".png")
-    #plt.clf()
-    #plt.title("")
-    
-    #n+=1
-    
-    return math.sqrt(statistics.mean(square_errors))/abs(data_diffs_mean) + sizepenalty*n_nodes(exp)
+    # plt.plot([i for i in range(0,36)], [interpreter.interpret(exp, {'X':i}) for i in range(0,36)])
+    """if random.random()<0.05:
+        print(expr_print(exp)+"\n"+str(math.sqrt(statistics.mean(square_errors))/abs(data_diffs_mean) + sizepenalty*n_nodes(exp)))"""
+    # plt.show()
+    # plt.savefig("GRPH"+str(n)+".png")
+    # plt.clf()
+    # plt.title("")
+
+    # n+=1
+
+    return math.sqrt(statistics.mean(square_errors)) / abs(
+        data_diffs_mean
+    ) + sizepenalty * n_nodes(exp)
+
 
 import random
 
-def randnum():
-    return random.random()*4-2
 
-        
+def randnum():
+    return random.random() * 4 - 2
+
+
 def nargs(func):
     return len(signature(func).parameters)
+
 
 def _handle(t):
     return t() if callable(t) else t
 
+
 def randexp(F, T, maxdepth=5):
     U = list(F.union(T))
+
     def _randexp(atom, depth=2):
         if atom in T:
             return _handle(atom)
         if atom in F:
-            return (atom, *[_randexp(
-                random.choice(U) if depth<maxdepth else random.choice(list(T)), depth+1
-                ) for _ in range(nargs(atom))])
+            return (
+                atom,
+                *[
+                    _randexp(
+                        random.choice(U)
+                        if depth < maxdepth
+                        else random.choice(list(T)),
+                        depth + 1,
+                    )
+                    for _ in range(nargs(atom))
+                ],
+            )
+
     return _randexp(random.choice(list(F)))
 
 
 def lnfunc():
-    return [log, [add, 'X', randexp(FUNCTIONS,TERMINALS,3)], randexp(FUNCTIONS,TERMINALS,3)]
+    return [
+        log,
+        [add, "X", randexp(FUNCTIONS, TERMINALS, 3)],
+        randexp(FUNCTIONS, TERMINALS, 3),
+    ]
+
 
 def powerfunc():
-    return [mul, [power, randexp(FUNCTIONS,TERMINALS,2), 'X'], randexp(FUNCTIONS,TERMINALS,2)]
+    return [
+        mul,
+        [power, randexp(FUNCTIONS, TERMINALS, 2), "X"],
+        randexp(FUNCTIONS, TERMINALS, 2),
+    ]
+
 
 def linearfunc():
-    return [mul, randexp(FUNCTIONS,TERMINALS,3), 'X']
+    return [mul, randexp(FUNCTIONS, TERMINALS, 3), "X"]
+
 
 def poly2func():
-    return [add, linearfunc(), [mul, randexp(FUNCTIONS,TERMINALS,3), [mul, 'X', 'X']]]
+    return [add, linearfunc(), [mul, randexp(FUNCTIONS, TERMINALS, 3), [mul, "X", "X"]]]
+
 
 def poly3func():
-    return [add, poly2func(), [mul, randexp(FUNCTIONS,TERMINALS,3), [mul, [mul, 'X', 'X'],'X']]]
+    return [
+        add,
+        poly2func(),
+        [mul, randexp(FUNCTIONS, TERMINALS, 3), [mul, [mul, "X", "X"], "X"]],
+    ]
+
 
 def poly4func():
-    return [add, poly3func(), [mul, randexp(FUNCTIONS,TERMINALS,3), [mul, [mul, [mul, 'X', 'X'],'X'],'X']]]
+    return [
+        add,
+        poly3func(),
+        [
+            mul,
+            randexp(FUNCTIONS, TERMINALS, 3),
+            [mul, [mul, [mul, "X", "X"], "X"], "X"],
+        ],
+    ]
 
-#evolve vectors for function types
+
+# evolve vectors for function types
 
 if args.suggest:
-    templates = ((1, {'linear':linearfunc,
-                'exponential':powerfunc,
-                'quadratic':poly2func,
-                'poly3':poly3func,
-                'poly4':poly4func,
-                'log':lnfunc}[args.suggest]),)
-    FUNCTIONS = {add,mul,sub}
-    TERMINALS = {randnum,1}
+    templates = (
+        (
+            1,
+            {
+                "linear": linearfunc,
+                "exponential": powerfunc,
+                "quadratic": poly2func,
+                "poly3": poly3func,
+                "poly4": poly4func,
+                "log": lnfunc,
+            }[args.suggest],
+        ),
+    )
+    FUNCTIONS = {add, mul, sub}
+    TERMINALS = {randnum, 1}
 else:
-    templates = ((1, 'RANDOM'),)
-    FUNCTIONS = {add,mul,sub,ln}
-    TERMINALS = {randnum,1,'X'}
+    templates = ((1, "RANDOM"),)
+    FUNCTIONS = {add, mul, sub, ln}
+    TERMINALS = {randnum, 1, "X"}
 
 if args.psize:
     psize = args.psize
@@ -252,7 +330,7 @@ else:
 solution = interpreter.evolve(
     functions=FUNCTIONS,
     terminals=TERMINALS,
-    fitness_function = lambda exp: score(exp),
+    fitness_function=lambda exp: score(exp),
     pop_size=psize,
     init_max_depth=3,
     crossover_rate=0.8,
@@ -260,33 +338,41 @@ solution = interpreter.evolve(
     mutation_rate=0.1,
     verbose=True,
     updatefreq=updatefreq,
-    templates = templates
-    )
+    templates=templates,
+)
 
 print("---")
 
-#use NRMSE
+# use NRMSE
 data_mean = statistics.mean(row[1] for row in data)
+
 
 def nscore(exp):
     square_errors = []
-    
-    data_to_score = [[pair[0],interpreter.interpret(exp, {'X':pair[0]})] for pair in data]
+
+    data_to_score = [
+        [pair[0], interpreter.interpret(exp, {"X": pair[0]})] for pair in data
+    ]
     try:
         for i, pair in enumerate(data_to_score):
-            square_errors.append((data_to_score[i][1]-data[i][1])**2)
+            square_errors.append((data_to_score[i][1] - data[i][1]) ** 2)
     except OverflowError:
-        return float('inf')
-        
-    return math.sqrt(statistics.mean(square_errors))/abs(data_mean) + 0.01*n_nodes(exp)
-    
+        return float("inf")
+
+    return math.sqrt(statistics.mean(square_errors)) / abs(data_mean) + 0.01 * n_nodes(
+        exp
+    )
+
+
 os.system("cls")
+os.system("clear")
+
 print("matching vertical shift...")
 
 C = interpreter.evolve(
-    functions={add,mul,sub,div,power},
-    terminals={randnum,1},
-    fitness_function = lambda exp: nscore([add,exp,solution]),
+    functions={add, mul, sub, div, power},
+    terminals={randnum, 1},
+    fitness_function=lambda exp: nscore([add, exp, solution]),
     pop_size=20,
     init_max_depth=3,
     crossover_rate=0.8,
@@ -294,15 +380,15 @@ C = interpreter.evolve(
     gens_cutoff=200,
     mutation_rate=0.1,
     verbose=False,
-    templates = ((1, 'RANDOM'),)
-    )
+    templates=((1, "RANDOM"),),
+)
 
 os.system("cls")
 
-x1=[]
-y1=[]
-x2=[]
-y2=[]
+x1 = []
+y1 = []
+x2 = []
+y2 = []
 
 
 for pair in data:
@@ -312,28 +398,34 @@ for pair in data:
 mx = max(pair[0] for pair in data)
 mn = min(pair[0] for pair in data)
 
-for x in range(0,1000):
-    x = (x/1000)*(mx-mn) + mn
+for x in range(0, 1000):
+    x = (x / 1000) * (mx - mn) + mn
     x2.append(x)
-    y2.append(interpreter.interpret([add,solution,C], {'X':x}))
+    y2.append(interpreter.interpret([add, solution, C], {"X": x}))
 
 
-plt.scatter(x1,y1, color='blue',marker='o')
-plt.plot(x2,y2, color='red', linestyle='--')
+plt.scatter(x1, y1, color="blue", marker="o")
+plt.plot(x2, y2, color="red", linestyle="--")
 plt.show()
 
 
-
-'''
+"""
 interpreter.graphprint([add,solution,C], "sol.gv")
 print()
 print(expr_print([add,solution,C]))
 print()
 print()
 print(expr_print(interpreter.simplify([add,solution,C])))
-print()'''
+print()"""
 
 print("found model f(X) = ")
 from sympy import latex, sympify
-print(latex(sympify(expr_print(interpreter.simplify([add,solution,C]))).expand().simplify()).replace("log","ln"))
+
+print(
+    latex(
+        sympify(expr_print(interpreter.simplify([add, solution, C])))
+        .expand()
+        .simplify()
+    ).replace("log", "ln")
+)
 print()
